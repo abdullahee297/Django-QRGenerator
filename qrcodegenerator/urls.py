634a4c688a0426eaf094b645b89646834d393fff
abdullahee297 <232841637+abdullahee297@users.noreply.github.com>
2026-04-25 19:41:43 +0500

    Deploy

--- qrcodegenerator/urls.py
+++ qrcodegenerator/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from turtle import home
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
